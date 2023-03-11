5f17c2d256f46a84d07364cb18564027c0c8e5c6
Gregory Foster <brona90@gmail.com>
2023-03-11 14:53:59 -0500

    using alias instead of function for config_setup.sh

--- config_setup.sh
+++ config_setup.sh
@@ -4,9 +4,7 @@ rm ${HOME}/.zshrc
 
 git clone --bare https://github.com/brona90/config.git ${HOME}/.cfg
 
-function config {
-  /usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME} $@
-}
+alias config="/usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME}"
 
 config checkout master
 config config status.showUntrackedFiles no
